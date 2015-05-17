f30c35adcc7102784c6e087524d082b57d59650b
Mihai Maruseac <mihai.maruseac@gmail.com>
2015-05-17 13:53:36 -0400

    Start adding properties

--- src/TinyHttpd.java
+++ src/TinyHttpd.java
@@ -1,9 +1,15 @@
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Properties;
 
 public class TinyHttpd {
-	private static final int PORT = 8888;
+	public static final Properties properties = readProperties();
+	private static final int PORT = Integer.parseInt(properties.getProperty("PORT", "8888"));
+
 	private ServerSocket serverSocket;
 
 	public void init() {
@@ -24,6 +30,19 @@ public class TinyHttpd {
 		} catch (IOException ex) {
 			ex.printStackTrace();
 		}
+	}
+
+	private static Properties readProperties() {
+		Properties properties = new Properties();
+
+		try {
+			InputStream in = new FileInputStream(new File("server.properties"));
+			properties.load(in);
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+
+		return properties;
 	}
 
 	public static void main(String[] args) {
