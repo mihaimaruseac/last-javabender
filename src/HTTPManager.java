ae23f2585f08a0169609766bb6c6a9658aa3a6a0
Mihai Maruseac <mihai.maruseac@gmail.com>
2015-05-17 00:22:52 -0400

    Remove STE problem

--- src/HTTPManager.java
+++ src/HTTPManager.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
 
 public class HTTPManager implements Runnable {
 	public Socket client;
@@ -34,6 +35,8 @@ public class HTTPManager implements Runnable {
 				client.close();
 				System.out.println("A connection is closed.");
 			}
+		} catch (SocketTimeoutException ex) {
+			/* ignore */
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
