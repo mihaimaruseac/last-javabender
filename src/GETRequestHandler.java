3564f6ce441208aa541219b703645da179e9e6a6
Mihai Maruseac <mihai.maruseac@gmail.com>
2015-05-16 23:02:49 -0400

    Style fixes

--- src/GETRequestHandler.java
+++ src/GETRequestHandler.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintStream;
-import java.io.File;
 
 public class GETRequestHandler extends ItemRequestHandler {
 	public GETRequestHandler(BufferedReader in, PrintStream out, String uri) {
--- src/HEADRequestHandler.java
+++ src/HEADRequestHandler.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
-import java.io.File;
 
 public class HEADRequestHandler extends ItemRequestHandler {
 	public HEADRequestHandler(BufferedReader in, PrintStream out, String uri) {
