f6aba858677cfaa98b61f8d3bfbbb23217a36f88
Mihai Maruseac <mihai.maruseac@gmail.com>
2015-05-17 20:57:50 -0400

    Basic SSL sockets

--- src/TinyHttpd.java
+++ src/TinyHttpd.java
@@ -6,22 +6,36 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Properties;
 
+import javax.net.ssl.SSLServerSocketFactory;
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLSocket;
+
 public class TinyHttpd {
 	public static final Properties properties = readProperties();
 	private static final int PORT = Integer.parseInt(properties.getProperty("PORT", "8888"));
 
-	private ServerSocket serverSocket;
+	//private ServerSocket serverSocket;
+	private SSLServerSocket serverSocket;
 
 	public void init() {
+		System.setProperty("javax.net.ssl.keyStoreType", "PKCS12");
+		System.setProperty("javax.net.ssl.keyStore", "key.cert.pcks12");
+		System.setProperty("javax.net.ssl.keyStorePassword", "Mihai");
 		try {
 			try {
-				serverSocket = new ServerSocket(PORT);
+				SSLServerSocketFactory factory = (SSLServerSocketFactory)SSLServerSocketFactory.getDefault();
+				serverSocket = (SSLServerSocket)factory.createServerSocket(PORT);
+				//serverSocket = new ServerSocket(PORT);
 				System.out.println("Server socket created.");
+
+				String enabledSuites[] = { "SSL_RSA_WITH_3DES_EDE_CBC_SHA" };
+				serverSocket.setEnabledCipherSuites(enabledSuites);
 
 				while (true) {
 					System.out.println("Listening to a connection on the local port " +
 							serverSocket.getLocalPort() + "...");
-					Socket client = serverSocket.accept();
+					//Socket client = serverSocket.accept();
+					SSLSocket client = (SSLSocket)serverSocket.accept();
 					StaticThreadPool.getInstance().execute(new HTTPManager(client));
 				}
 			} finally {
